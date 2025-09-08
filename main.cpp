4779529772ff9daf18efe6e9cccc8b6ca3e7788f
Degen1 <98870418+Degen1@users.noreply.github.com>
2025-09-08 10:03:00 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>   // For input and output
+
+using namespace std;
+
+int main() {
+    // Print a message
+    cout << "Hello, World!" << endl;
+
+    // Variables
+    int a = 5;
+    int b = 2;
+    int sum = a + b;
+
+    // Show result
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "Sum = " << sum << endl;
+
+    return 0; // End program
+}
